b510898603616da173932f4dc9ef3ac28df86fd2
harvestcore <aagomezies@gmail.com>
2019-04-21 12:41:08 +0200

    expo twig

---
+++ twig/html/indexdemo3.php
@@ -0,0 +1,9 @@
+<?php
+    require_once './vendor/autoload.php';
+
+    $loader = new \Twig\Loader\FilesystemLoader('./views');
+    $twig = new \Twig\Environment($loader);
+    
+    echo $twig->render('base3.html', []);
+
+?>
