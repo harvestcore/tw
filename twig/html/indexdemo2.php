ebd92aa0e294852c289f3c5bbcbdafedc6079771
harvestcore <aagomezies@gmail.com>
2019-06-20 21:45:53 +0200

    quito expo twig

--- twig/html/indexdemo2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once './vendor/autoload.php';
-
-    $loader = new \Twig\Loader\FilesystemLoader('./views');
-    $twig = new \Twig\Environment($loader);
-    
-    echo $twig->render('base2.html', ['nombre' => 'Ángel']);
-?>
--- twig/html/indexdemo3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once './vendor/autoload.php';
-
-    $loader = new \Twig\Loader\FilesystemLoader('./views');
-    $twig = new \Twig\Environment($loader);
-    
-    echo $twig->render('base3.html', []);
-
-?>
